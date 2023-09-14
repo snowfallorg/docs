8a5536e9617c24a0c0842228c57e85eb3bc6a00b
Jake Hamilton <jake.hamilton@hey.com>
2023-09-13 23:22:10 -0700

    feat: add exported nix package

--- flake.nix
+++ flake.nix
@@ -32,21 +32,25 @@
           # NOTE: This is currently broken due to esbuild complaining about a mismatched
           # binary version. This seems to be caused by vite specifying exactly version 0.18.20
           # regardless of what this package may require.
-          snowfall-docs = channels.nixpkgs.buildNpmPackage {
-            pname = "snowfall-docs";
-            src = ./.;
-            version = inputs.self.sourceInfo.shortRev or "dirty";
+          # snowfall-docs = channels.nixpkgs.buildNpmPackage {
+          #   pname = "snowfall-docs";
+          #   src = ./.;
+          #   version = inputs.self.sourceInfo.shortRev or "dirty";
 
-            npmDepsHash = "sha256-sLKRNWwhRb9uyxJHC5xWC7PKl1nBj30RFgBm7m5dzgc=";
+          #   npmDepsHash = "sha256-sLKRNWwhRb9uyxJHC5xWC7PKl1nBj30RFgBm7m5dzgc=";
 
-            npmFlags = ["--ignore-scripts" "--legacy-peer-deps"];
+          #   npmFlags = ["--ignore-scripts" "--legacy-peer-deps"];
 
-            ESBUILD_BINARY_PATH = "${channels.nixpkgs.esbuild}/bin/esbuild";
+          #   ESBUILD_BINARY_PATH = "${channels.nixpkgs.esbuild}/bin/esbuild";
 
-            installPhase = ''
-              mv dist $out
-            '';
-          };
+          #   installPhase = ''
+          #     mv dist $out
+          #   '';
+          # };
+
+          snowfall-docs = channels.nixpkgs.runCommandNoCC "snowfall-docs" {} ''
+            cp -r ${./dist} $out
+          '';
         };
       };
     };
