9b3cafe1d372bc6082aece30bfa0a87a78bc1ad6
Jake Hamilton <jake.hamilton@hey.com>
2023-12-09 08:39:45 -0800

    feat: new home page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,37 +1,42 @@
-import { defineConfig } from "astro/config";
-import starlight from "@astrojs/starlight";
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     starlight({
-      title: "Snowfall",
-      customCss: ["./src/styles/theme.css"],
+      title: 'Snowfall',
+      favicon: './src/assets/snowfall.jpg',
+      customCss: [
+        '@fontsource/nunito/400.css',
+        '@fontsource/nunito/700.css',
+        './src/styles/theme.css',
+      ],
       social: {
-        github: "https://github.com/snowfallorg",
+        github: 'https://github.com/snowfallorg',
       },
       sidebar: [
         {
-          label: "Lib",
+          label: 'Lib',
           items: [
-            { label: "Quickstart", link: "/guides/lib/quickstart/" },
+            { label: 'Quickstart', link: '/guides/lib/quickstart/' },
             {
-              label: "Guides",
+              label: 'Guides',
               items: [
-                { label: "Packages", link: "/guides/lib/packages/" },
-                { label: "Overlays", link: "/guides/lib/overlays/" },
-                { label: "Modules", link: "/guides/lib/modules/" },
-                { label: "Systems", link: "/guides/lib/systems/" },
-                { label: "Homes", link: "/guides/lib/homes/" },
-                { label: "Library", link: "/guides/lib/library/" },
-                { label: "Shells", link: "/guides/lib/shells/" },
-                { label: "Generic", link: "/guides/lib/generic/" },
+                { label: 'Packages', link: '/guides/lib/packages/' },
+                { label: 'Overlays', link: '/guides/lib/overlays/' },
+                { label: 'Modules', link: '/guides/lib/modules/' },
+                { label: 'Systems', link: '/guides/lib/systems/' },
+                { label: 'Homes', link: '/guides/lib/homes/' },
+                { label: 'Library', link: '/guides/lib/library/' },
+                { label: 'Shells', link: '/guides/lib/shells/' },
+                { label: 'Generic', link: '/guides/lib/generic/' },
               ],
             },
-            { label: "Reference", link: "/reference/lib/" },
+            { label: 'Reference', link: '/reference/lib/' },
             {
-              label: "Migration",
-              items: [{ label: "v2", link: "/guides/lib/migration/v2/" }],
+              label: 'Migration',
+              items: [{ label: 'v2', link: '/guides/lib/migration/v2/' }],
             },
           ],
         },
@@ -40,6 +45,9 @@ export default defineConfig({
         //   autogenerate: { directory: "reference" },
         // },
       ],
+      components: {
+        Hero: './src/components/Hero.astro',
+      },
     }),
   ],
 });
