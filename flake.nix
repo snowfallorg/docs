5240afab60494a6ffba4398a586db911cf11c128
Jake Hamilton <jake.hamilton@hey.com>
2024-02-23 09:28:50 -0800

    chore: remove unused code

--- flake.nix
+++ flake.nix
@@ -21,44 +21,5 @@
       };
 
       alias.packages.default = "snowfall-docs";
-
-      # outputs-builder = channels: {
-      #   devShells = {
-      #     default = channels.nixpkgs.mkShell {
-      #       ESBUILD_BINARY_PATH = "${channels.nixpkgs.esbuild}/bin/esbuild";
-
-      #       packages = with channels.nixpkgs; [
-      #         esbuild
-      #       ];
-      #     };
-      #   };
-
-      #   packages = rec {
-      #     default = snowfall-docs;
-
-      #     # NOTE: This is currently broken due to esbuild complaining about a mismatched
-      #     # binary version. This seems to be caused by vite specifying exactly version 0.18.20
-      #     # regardless of what this package may require.
-      #     # snowfall-docs = channels.nixpkgs.buildNpmPackage {
-      #     #   pname = "snowfall-docs";
-      #     #   src = ./.;
-      #     #   version = inputs.self.sourceInfo.shortRev or "dirty";
-
-      #     #   npmDepsHash = "sha256-sLKRNWwhRb9uyxJHC5xWC7PKl1nBj30RFgBm7m5dzgc=";
-
-      #     #   npmFlags = ["--ignore-scripts" "--legacy-peer-deps"];
-
-      #     #   ESBUILD_BINARY_PATH = "${channels.nixpkgs.esbuild}/bin/esbuild";
-
-      #     #   installPhase = ''
-      #     #     mv dist $out
-      #     #   '';
-      #     # };
-
-      #     snowfall-docs = channels.nixpkgs.runCommandNoCC "snowfall-docs" {} ''
-      #       cp -r ${./dist} $out
-      #     '';
-      #   };
-      # };
     };
 }
