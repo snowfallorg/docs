4384dd619b66e7129ceccdb5fc66c07fa61e17a7
Jake Hamilton <jake.hamilton@hey.com>
2024-02-24 08:04:30 -0800

    feat: add initial v3 migration guide

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,52 +3,55 @@ import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    starlight({
-      title: "Snowfall",
-      favicon: "./src/assets/snowfall.jpg",
-      customCss: [
-        "@fontsource/nunito/400.css",
-        "@fontsource/nunito/700.css",
-        "./src/styles/theme.css",
-      ],
-      social: {
-        github: "https://github.com/snowfallorg",
-      },
-      sidebar: [
-        {
-          label: "Lib",
-          items: [
-            { label: "Quickstart", link: "/guides/lib/quickstart/" },
-            {
-              label: "Guides",
-              items: [
-                { label: "Packages", link: "/guides/lib/packages/" },
-                { label: "Overlays", link: "/guides/lib/overlays/" },
-                { label: "Modules", link: "/guides/lib/modules/" },
-                { label: "Systems", link: "/guides/lib/systems/" },
-                { label: "Homes", link: "/guides/lib/homes/" },
-                { label: "Library", link: "/guides/lib/library/" },
-                { label: "Shells", link: "/guides/lib/shells/" },
-                { label: "Generic", link: "/guides/lib/generic/" },
-                { label: "Channels", link: "/guides/lib/channels/" },
-              ],
-            },
-            { label: "Reference", link: "/reference/lib/" },
-            {
-              label: "Migration",
-              items: [{ label: "v2", link: "/guides/lib/migration/v2/" }],
-            },
-          ],
-        },
-        // {
-        //   label: "Reference",
-        //   autogenerate: { directory: "reference" },
-        // },
-      ],
-      components: {
-        Hero: "./src/components/hero.astro",
-      },
-    }),
-  ],
+	integrations: [
+		starlight({
+			title: "Snowfall",
+			favicon: "./src/assets/snowfall.jpg",
+			customCss: [
+				"@fontsource/nunito/400.css",
+				"@fontsource/nunito/700.css",
+				"./src/styles/theme.css",
+			],
+			social: {
+				github: "https://github.com/snowfallorg",
+			},
+			sidebar: [
+				{
+					label: "Lib",
+					items: [
+						{ label: "Quickstart", link: "/guides/lib/quickstart/" },
+						{
+							label: "Guides",
+							items: [
+								{ label: "Packages", link: "/guides/lib/packages/" },
+								{ label: "Overlays", link: "/guides/lib/overlays/" },
+								{ label: "Modules", link: "/guides/lib/modules/" },
+								{ label: "Systems", link: "/guides/lib/systems/" },
+								{ label: "Homes", link: "/guides/lib/homes/" },
+								{ label: "Library", link: "/guides/lib/library/" },
+								{ label: "Shells", link: "/guides/lib/shells/" },
+								{ label: "Generic", link: "/guides/lib/generic/" },
+								{ label: "Channels", link: "/guides/lib/channels/" },
+							],
+						},
+						{ label: "Reference", link: "/reference/lib/" },
+						{
+							label: "Migration",
+							items: [
+								{ label: "v2", link: "/guides/lib/migration/v2/" },
+								{ label: "v3", link: "/guides/lib/migration/v3/" },
+							],
+						},
+					],
+				},
+				// {
+				//   label: "Reference",
+				//   autogenerate: { directory: "reference" },
+				// },
+			],
+			components: {
+				Hero: "./src/components/hero.astro",
+			},
+		}),
+	],
 });
